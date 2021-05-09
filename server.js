6b2633ca4fa30c9e13f7c64394d8578dfc9e24db
japankid-code <japankid.jake@gmail.com>
2021-05-09 11:41:37 -0500

    create CRUD routes for database, close #1

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 
@@ -19,8 +20,79 @@ const db = mysql.createConnection(
   console.log("connected to the election database!!")
 );
 
-db.query('SELECT * FROM candidates', (err, rows) => {
-  console.log(rows);
+// get all candidates
+app.get('/api/candidates', (req, res) => {
+  sql = 'SELECT * FROM candidates';
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status.json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    })
+  })
+})
+
+// select individual candidates
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = 'SELECT * FROM candidates WHERE id = ?'
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    })
+  })
+
+})
+
+// deleting candidates
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected')
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+                VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    })
+  })
 })
 
 // handle unsupported requests
